f237bf371a357e2681ba8596795d34172916cf30
Jose Armesto <github@armesto.net>
2017-04-30 17:25:24 +0200

    Delete ECR. Update to 0.4.0

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ export TAG=`if [ -z "${TRAVIS_TAG}" ]; then echo "latest"; else echo "${TRAVIS_T
 
 docker login -e "${DOCKER_EMAIL}" -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
 docker build -t ${REPO}:latest .
-docker tag $REPO:latest $REPO:$TAG
-docker push $REPO
+docker tag ${REPO}:latest ${REPO}:${TAG}
+docker push ${REPO}
