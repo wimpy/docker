ee57eb829bede168236051386b8bb14774f7808d
Jose Armesto <github@armesto.net>
2017-05-12 14:15:22 +0200

    Forget about gnu parallel

--- test.sh
+++ test.sh
@@ -2,20 +2,50 @@
 
 set -euv
 
-export DEPLOY_CMD='docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/app" -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY fiunchinho/wimpy /app/deploy.yml --extra-vars "wimpy_release_version=`git rev-parse HEAD` wimpy_deployment_environment=production" -vv'
-mkdir workspace
-cd workspace
+function deploy_first_canary() {
+    git clone https://github.com/wimpy/canary.git
+    cd canary
+    docker run --rm -it \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v "$PWD:/app" \
+        -e AWS_ACCESS_KEY_ID \
+        -e AWS_SECRET_ACCESS_KEY \
+        fiunchinho/wimpy /app/deploy.yml  \
+          --extra-vars "wimpy_release_version=`git rev-parse HEAD` wimpy_deployment_environment=production" -vv
 
-git clone https://github.com/wimpy/canary.git --branch master master
-git clone https://github.com/wimpy/canary.git --branch blue_green blue_green
+    curl --fail canary.armesto.net/health
+    cd ..
+}
 
-ls -d ./* | parallel -j2 --gnu "cd {} && $DEPLOY_CMD"
+function deploy_second_canary() {
+    git clone https://github.com/wimpy/canary.git canary2
+    cd canary2
+    git checkout blue_green && docker run --rm -it \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v "$PWD:/app" \
+        -e AWS_ACCESS_KEY_ID \
+        -e AWS_SECRET_ACCESS_KEY \
+        fiunchinho/wimpy /app/deploy.yml  \
+          --extra-vars "wimpy_release_version=`git rev-parse HEAD^1` wimpy_deployment_environment=production" -vv
 
-curl --fail canary.armesto.net/healthz
-curl --fail canary2.armesto.net/healthz
+    curl --fail canary2.armesto.net/healthz
+    cd ..
+}
 
-# Re Deploy
-cd blue_green
-git checkout HEAD^1
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/app" -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY fiunchinho/wimpy /app/deploy.yml --extra-vars "wimpy_release_version=`git rev-parse HEAD` wimpy_deployment_environment=production" -vv
-curl --fail canary2.armesto.net/healthz
+function deploy_third_canary() {
+    cd canary2
+    docker run --rm -it \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v "$PWD:/app" \
+        -e AWS_ACCESS_KEY_ID \
+        -e AWS_SECRET_ACCESS_KEY \
+        fiunchinho/wimpy /app/deploy.yml  \
+          --extra-vars "wimpy_release_version=`git rev-parse HEAD` wimpy_deployment_environment=production" -vv
+
+    curl --fail canary2.armesto.net/healthz
+    cd ..
+}
+
+deploy_first_canary
+deploy_second_canary
+deploy_third_canary
